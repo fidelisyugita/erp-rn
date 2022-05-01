c98af9ff2e41d9418cd3d9b7a24eb61775a5d657
Muhamad Iqbal <mhmdiqbalabqi@gmail.com>
2022-05-01 11:17:19 +0700

    wip dashboard

---
+++ src/Screens/Dashboard/Components/FilterDashboard.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import {
+  Box,
+  CheckIcon,
+  HStack,
+  IconButton,
+  Input,
+  ScrollView,
+  Select,
+  Skeleton,
+  Text,
+  useDisclose,
+} from 'native-base'
+import { useTranslation } from 'react-i18next'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { DateRange } from '@/Components/Atoms'
+import { dateRangeConverter } from '@/Helper/DateHelper'
+import {
+  useGetBuyingStatusQuery,
+  useGetBuyingTypeQuery,
+} from '@/Services/modules/buying'
+
+const FilterDasbhoard = ({ setFilterDate, filterDate }) => {
+  const { t } = useTranslation()
+  const { isOpen, onOpen, onClose } = useDisclose()
+
+  return (
+    <>
+      <DateRange
+        isOpen={isOpen}
+        onClose={onClose}
+        setRange={setFilterDate}
+        selectedRange={filterDate}
+      />
+      <HStack px="4" mt="2" alignItems={'center'}>
+        <Text fontSize="20" mr="4">
+          {t('periode')}
+        </Text>
+        <Input
+          maxWidth="1/2"
+          value={dateRangeConverter(filterDate)}
+          backgroundColor="white"
+          isDisabled
+          InputRightElement={
+            <IconButton
+              onPress={onOpen}
+              _icon={{
+                as: MaterialCommunityIcons,
+                name: 'calendar',
+                size: '6',
+              }}
+            />
+          }
+        />
+      </HStack>
+    </>
+  )
+}
+
+export default FilterDasbhoard
--- src/Screens/Dashboard/DashboardScreen.js
+++ src/Screens/Dashboard/DashboardScreen.js
@@ -1,8 +1,53 @@
-import React from 'react'
-import { Box } from 'native-base'
+import React, { useState } from 'react'
+import { Box, Text, VStack, Divider, CheckIcon, Select } from 'native-base'
+import moment from 'moment'
+import FilterDasbhoard from './Components/FilterDashboard'
+import { useTranslation } from 'react-i18next'
 
 const DashboardScreen = () => {
-  return <Box flex="1" />
+  const { t } = useTranslation()
+  const [filterDate, setFilterDate] = useState({
+    firstDate: moment().format('YYYY-MM-DD'),
+    secondDate: moment().format('YYYY-MM-DD'),
+  })
+  return (
+    <Box flex="1" backgroundColor="white">
+      <FilterDasbhoard filterDate={filterDate} setFilterDate={setFilterDate} />
+      <VStack
+        space="2"
+        mt="4"
+        mx={'4'}
+        borderWidth="1"
+        borderColor="gray.200"
+        p="2"
+        borderRadius="md"
+        mb="4"
+      >
+        <Text>{t('keuntungan')}: </Text>
+        <Text>{t('penjualan')}: </Text>
+        <Text>{t('reject')}: </Text>
+      </VStack>
+
+      <Divider />
+      <Select
+        selectedValue={sort}
+        minWidth="100"
+        h="12"
+        placeholder={t('sortBy')}
+        onValueChange={setSort}
+        mr="1"
+        _selectedItem={{
+          bg: 'primary.500',
+          endIcon: <CheckIcon size="5" />,
+        }}
+      >
+        <Select.Item label={t('allStatus')} value="" />
+        {buyingStatus.map(ts => (
+          <Select.Item key={String(ts.id)} label={ts.name} value={ts.id} />
+        ))}
+      </Select>
+    </Box>
+  )
 }
 
 export default DashboardScreen
