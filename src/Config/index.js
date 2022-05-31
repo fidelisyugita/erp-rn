f382c8e92ff2f1864569d5e5cebdfd3a619257bf
Fidelis Yugita <fb46us@gmail.com>
2022-05-31 16:00:44 +0700

    update baseUrl

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,3 +1,4 @@
 export const Config = {
-  API_URL: 'https://asia-southeast2-erp-firebase-4e1a2.cloudfunctions.net/',
+  // API_URL: 'https://asia-southeast2-erp-firebase-4e1a2.cloudfunctions.net/',
+  API_URL: 'https://asia-southeast2-kreasindo-mitra-prima.cloudfunctions.net/',
 }
